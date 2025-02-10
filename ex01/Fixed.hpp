cfa11cb957d8820dbad8a275ca5dcaec5de9e8c1
haalouan <haalouan@student.1337.ma>
2025-02-10 21:04:23 +0100

    push

---
+++ ex01/Fixed.hpp
@@ -0,0 +1,30 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+class Fixed{
+
+    private:
+        int fixedValue;
+        static const int fractionalBits = 8;
+    
+    public:
+        Fixed();
+        Fixed(const int value);
+        Fixed(const float value);
+        Fixed(const Fixed& other);
+        ~Fixed();
+
+        Fixed& operator=(const Fixed& other);
+        
+        int getRawBits( void ) const;
+        void setRawBits( int const raw );
+        
+        float toFloat( void ) const;
+        int toInt( void ) const;
+    };
+    std::ostream& operator<<(std::ostream& os, const Fixed& obj);
+
+#endif
